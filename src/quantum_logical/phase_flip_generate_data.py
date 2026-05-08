bed8bce59741096771d9bd8540c3505ed2a1dca9
Wrn13 <alphwrn@gmail.com>
2026-05-08 13:55:16 -0400

    WIP data generation

--- src/quantum_logical/phase_flip_generate_data.py
+++ src/quantum_logical/phase_flip_generate_data.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import qutip as qt
 
-from quantum_logical.experiment_runner import ExperimentRunner
 from quantum_logical.channel import AmplitudeDamping
+from quantum_logical.experiment_runner import ExperimentRunner
+from quantum_logical.qubit_phase import (
+    qubit_parallel_phase_circuit,
+    qubit_partial_phase_circuit,
+)
 from quantum_logical.trotter import TrotterGroup
 
 
-from quantum_logical.qubit_phase import qubit_parallel_phase_circuit, qubit_partial_phase_circuit
-
-def single_run_n_iterations(n:int):
+def single_run_n_iterations(n:int, init_rho:qt.Qobj, T1:float, T2:float, single_qubit_time:float, two_qubit_time:float, runner:ExperimentRunner):
 
     setup_5 = [
     qubit_parallel_phase_circuit(single_qubit_time, two_qubit_time, num_ancillae=2)
@@ -44,37 +46,56 @@ def single_run_n_iterations(n:int):
         ideal_dropped_states_5 += runner.run_unitary_circuit(trotter_5, [qt.tensor(*[qt.qeye(2)]*5)], state_list_5[19 * i], [19 * single_qubit_time])[1:-1]
         ideal_dropped_states_5.append(state_list_5[19*(i+1)])
     
-    no_correction = runner.run_unitary_circuit(trotter_4, [qt.tensor(*[qt.qeye(2)]*4)], qt.tensor(init_rho, ancilla_zero), [len(ideal_dropped_states_4) * single_qubit_time]) [:-1]   
+    no_correction = runner.run_unitary_circuit(trotter_4, [qt.tensor(*[qt.qeye(2)]*4)], qt.tensor(init_rho, ancilla_zero), [len(ideal_dropped_states_4) * single_qubit_time]) [:-1]  
+
+    return no_correction, ideal_dropped_states_4, ideal_dropped_states_5 
 
 
+def generate_haar_random_logical_state(zero_logical, one_logical):
+    coeffs = np.random.rand(2) + 1j * np.random.rand(2)
+    coeffs /= np.linalg.norm(coeffs)
+    print(coeffs)
+    return coeffs[0] * zero_logical + coeffs[1] * one_logical
 
 
-single_qubit_time = 0.03 # 30 nanosec
-two_qubit_time = 0.102 # 102 nanosec
+def main():
+    single_qubit_time = 0.03 # 30 nanosec
+    two_qubit_time = 0.102 # 102 nanosec
 
 
-# Define starting states
-plus_state = qt.gates.snot() * qt.basis(2,0)
-minus_state = qt.gates.snot() * qt.basis(2,1)
-zero_logical = qt.tensor(plus_state, plus_state, plus_state)
-one_logical = qt.tensor(minus_state, minus_state, minus_state)
-plus_logical = 1/np.sqrt(2) * (zero_logical + one_logical)
-minus_logical = 1/np.sqrt(2) * (zero_logical - one_logical)
- 
-runner = ExperimentRunner()
+    # Define starting states
+    plus_state = qt.gates.snot() * qt.basis(2,0)
+    minus_state = qt.gates.snot() * qt.basis(2,1)
+    zero_logical = qt.tensor(plus_state, plus_state, plus_state)
+    one_logical = qt.tensor(minus_state, minus_state, minus_state)
+    plus_logical = 1/np.sqrt(2) * (zero_logical + one_logical)
+    i_logical = 1/np.sqrt(2) * (zero_logical + 1j * one_logical)
+    minus_i_logical = 1/np.sqrt(2) * (zero_logical - 1j * one_logical)
+    minus_logical = 1/np.sqrt(2) * (zero_logical - one_logical)
+    
+    runner = ExperimentRunner()
 
-T1_list = [280, 280, 100]
-T2_list = [100, 330, 330]
+    T1_list = [280, 280, 100]
+    T2_list = [100, 330, 330]
 
-initial_states = [zero_logical, one_logical, plus_logical, minus_logical]
-initial_state_names = [r"$\ket{0}_L", r"$\ket{1}_L", r"$\ket{+}_L", r"$\ket{-}_L" ]
-for T1,T2 in zip(T1_list, T2_list):
-    for initial_state, initial_state_name in zip(initial_states, initial_state_names):
-        # Create the initial density matrices
-        initial_rho_4q = qt.ket2dm(qt.tensor(initial_state, qt.basis(2,0)))
-        initial_rho_5q = qt.ket2dm(qt.tensor(initial_state, qt.basis(2,0), qt.basis(2,0)))
+    initial_states = [zero_logical, one_logical, plus_logical, minus_logical, i_logical, minus_i_logical]
+    
+    initial_state_names = [r"$\ket{0}_L$", r"$\ket{1}_L$", r"$\ket{+}_L$", r"$\ket{-}_L$", r"$\ket{i}_L$", r"$\ket{-i}_L$" ]
 
-        print(f"Running T1 = {T1}, T2 = {T2} for starting state {initial_state_name}")
+    print("Running orthogonal states:")
+    for T1,T2 in zip(T1_list, T2_list):
+        for initial_state, initial_state_name in zip(initial_states, initial_state_names):
+            # Create the initial density matrices
+            initial_rho_4q = qt.ket2dm(qt.tensor(initial_state, qt.basis(2,0)))
+            initial_rho_5q = qt.ket2dm(qt.tensor(initial_state, qt.basis(2,0), qt.basis(2,0)))
+
+            print(f"Running T1 = {T1}, T2 = {T2} for starting state {initial_state_name}")
+
+    num_initial_states = 16
+    print("Running Haar random states:")
+if __name__ == "__main__":
+    main()
+
 
 
 
